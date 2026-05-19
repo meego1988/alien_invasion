2d6f5ac051c7ddf598d43c786e2c1be3aff4489e
meego1988 <963845827@qq.com>
2026-05-19 12:05:03 +0800

    Add(ship.py):增加新文件ship.py并编写Ship类

---
+++ ship.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Ship:
+    """管理飞船的类"""
+    def __init__(self, ai_game):
+        """初始化飞船并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 飞船的位置设置在屏幕底部的中间
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """在指定位置"""
